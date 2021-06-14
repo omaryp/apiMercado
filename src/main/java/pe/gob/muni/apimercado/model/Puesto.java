2625363e27de032e42df731bbbaf84d12f32cca0
Omar Yarlequé <omaryar7.12@gmail.com>
2021-06-13 22:31:55 -0500

    Se modificó propiedades de la clase Puesto

--- src/main/java/pe/gob/muni/apimercado/model/Puesto.java
+++ src/main/java/pe/gob/muni/apimercado/model/Puesto.java
@@ -1,20 +1,16 @@
 package pe.gob.muni.apimercado.model;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class Puesto extends BasicEntity {
 	
-	@NotBlank(message="Atributo codigo no debe ser vacío.")
 	@NotNull(message="Atributo codigo no debe ser vacío.")
 	private String codigo;
 	@Min(value=1, message="Atributo mercado debe ser != 0")
-	private int mercado_id;
+	private int mercados_id;
 	@Min(value=1, message="Atributo concepto debe ser != 0")
 	private int conceptos_id;
-	@Min(value=1, message="Atributo cobrador debe ser != 0")
-	private int cobradores_id;
 	@Min(value=1, message="Atributo ubicación debe ser != 0")
 	private int ubicaciones_id;
 	@Min(value=1, message="Atributo giro debe ser != 0")
@@ -26,23 +22,17 @@ public class Puesto extends BasicEntity {
 	public void setCodigo(String codigo) {
 		this.codigo = codigo;
 	}
-	public int getMercado_id() {
-		return mercado_id;
+	public int getMercados_id() {
+		return mercados_id;
 	}
-	public void setMercado_id(int mercado_id) {
-		this.mercado_id = mercado_id;
+	public void setMercados_id(int mercados_id) {
+		this.mercados_id = mercados_id;
 	}
 	public int getConceptos_id() {
 		return conceptos_id;
 	}
 	public void setConceptos_id(int conceptos_id) {
 		this.conceptos_id = conceptos_id;
-	}
-	public int getCobradores_id() {
-		return cobradores_id;
-	}
-	public void setCobradores_id(int cobradores_id) {
-		this.cobradores_id = cobradores_id;
 	}
 	public int getUbicaciones_id() {
 		return ubicaciones_id;
@@ -56,7 +46,5 @@ public class Puesto extends BasicEntity {
 	public void setGiro_id(int giro_id) {
 		this.giro_id = giro_id;
 	}
-	
-	
 	
 }
