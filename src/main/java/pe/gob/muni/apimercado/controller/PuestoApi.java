fb07e9275074e8b9ac72061e8428ebe31d8fcb99
Omar Yarlequé <omaryar7.12@gmail.com>
2021-06-29 18:51:33 -0500

    Se corrigio la asocición de puesto comerciante

--- src/main/java/pe/gob/muni/apimercado/controller/PuestoApi.java
+++ src/main/java/pe/gob/muni/apimercado/controller/PuestoApi.java
@@ -1,5 +1,6 @@
 package pe.gob.muni.apimercado.controller;
 
+import static pe.gob.muni.apimercado.utils.Constants.DATOS_NO_VALIDOS;
 import static pe.gob.muni.apimercado.utils.Constants.ERROR_AL_PROCESAR_PETICION;
 import static pe.gob.muni.apimercado.utils.Constants.ERROR_INTERNO;
 import static pe.gob.muni.apimercado.utils.Constants.TRANSACCION_OK;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,22 +27,25 @@ import pe.gob.muni.apimercado.model.dto.PuestoDto;
 import pe.gob.muni.apimercado.service.IPuestoService;
 import pe.gob.muni.apimercado.utils.ApiException;
 import pe.gob.muni.apimercado.utils.Util;
+import pe.gob.muni.apimercado.utils.ValidatorException;
 
 @RestController
 @RequestMapping("/puesto")
 public class PuestoApi extends BasicController<Puesto, IPuestoService> {
 
 	@PostMapping("/comerciante")
-	public ResponseEntity<?> asociarPuestoComerciante(PuestoComerciante entity) {
+	public ResponseEntity<?> asociarPuestoComerciante(@RequestBody PuestoComerciante entity) {
 		logger.info("Se recibió puesto comerciante - {}",Util.objectToJson(entity));
 		try {
 			service.asociarPuestoComerciante(entity);
 			return respuestaApi(null, "Transacción OK.", TRANSACCION_OK, HttpStatus.OK);
+		} catch (ValidatorException e) {
+			logger.error("Errores de validacion en la solicitud de  guardar  - {}",Util.objectToJson(entity));
+			return respuestaApi(e.getErrores(), e.getMessage(), DATOS_NO_VALIDOS, HttpStatus.BAD_REQUEST);
 		} catch (ApiException e) {
 			logger.error("Error de api al procesar peticion guardar - {} - {}",e.getMessage(),e);
 			return respuestaApi(null, e.getMessage(), ERROR_AL_PROCESAR_PETICION, HttpStatus.ACCEPTED);
-		} 
-		catch (Exception e) {
+		} catch (Exception e) {
 			logger.error("Error interno de api al procesar guardar - {}- {}",e.getMessage(),e);
 			return respuestaApi(null, e.getMessage(), ERROR_INTERNO, HttpStatus.INTERNAL_SERVER_ERROR);	
 		}
