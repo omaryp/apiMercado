553ee26df2e1714031316c494a701cda4924e25a
Omar Yarlequé <omaryar7.12@gmail.com>
2021-08-23 00:57:09 -0500

    Corrigiendo inicio de session

--- src/main/java/pe/gob/muni/apimercado/utils/Constants.java
+++ src/main/java/pe/gob/muni/apimercado/utils/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
 	public static final String LOGIN_URL = "/login";
 	public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
 	public static final String TOKEN_BEARER_PREFIX = "Bearer ";
-	public static final String SECRET_PASSWORD = "..::[{?*?*?#####SECRET#####?*?*?}]::..";
+	public static final String SECRET_PASSWORD = "************#####SECRET#####************";
 
 	// JWT
 
